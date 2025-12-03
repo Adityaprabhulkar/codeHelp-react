3e141ad3804dc81edee872264ae2803e2dfb9681
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-03 20:52:48 +0530

    learn about react-redux, redu toolkit how use it and learn flow

---
+++ react-redux-toolkit/src/App.jsx
@@ -0,0 +1,52 @@
+
+
+import { useDispatch,useSelector } from 'react-redux'
+import {useState} from 'react'
+import './App.css'
+import { decrement, increment , reset, incrementByAmount} from './features/counter/counterSlice';
+
+
+function App() {
+   const [amount, setAmount] = useState(0)
+   const count = useSelector((state) => state.counter.value);
+   const dispatch = useDispatch();
+
+  function handleIncrementClick() {
+      dispatch(increment());
+  }
+
+  function handleDecrementClick() {
+      dispatch(decrement());
+  }
+
+  function handleResetClick() {
+      dispatch(reset());
+  }
+
+  function handleIncAmount() {
+    dispatch(incrementByAmount(amount))
+  }
+  
+
+  return (
+  <div className='container'>
+    <button onClick={handleIncrementClick}> + </button>
+    <p>Count: {count}</p>
+    <button onClick={handleDecrementClick}> - </button>
+    <br/>
+    <br/> 
+    <button onClick={handleResetClick}> Reset </button>
+    <br/>
+    <br/>
+    <input type='number'
+    value={amount}
+    placeholder='Enter Ammount'
+    onChange={(e) => setAmount(e.target.value)} /><br/>
+    <br/> 
+    
+    <button onClick={handleIncAmount}> Inc by Amount </button>
+  </div>
+  )
+}
+
+export default App
